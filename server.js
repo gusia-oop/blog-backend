b74c06e9e065828db05b422679794e857e9f6bab
WIN-1OPER19RNES\beastbzz <anara.baidulla>
2020-11-06 16:04:50 +0600

    finished project

---
+++ server.js
@@ -0,0 +1,27 @@
+// Blog import
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./routes/models/article')
+const methodOverride = require('method-override')
+const app = express()
+const articleRouter = require('./routes/articles')
+
+mongoose.connect('mongodb://localhost/orayTechnics_blog', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({ createdAt: 'desc' })
+    res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+
+app.listen(3000)
